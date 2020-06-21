2536d9402da6874ce3959d116a4d69dea758e215
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-21 10:55:45 +0530

    login username

--- src/hotel/management/system/Login.java
+++ src/hotel/management/system/Login.java
@@ -4,20 +4,32 @@ package hotel.management.system;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 
 public class Login extends JFrame {
     
     JLabel l1,l2;
-    JTextField t1,t2;
+    JTextField t1;
+    JPasswordField t2;
     JButton b1,b2;
     
     Login(){
+        
         l1 = new JLabel("Username");
         l1.setBounds(40, 20, 100, 30);
         add(l1);
         
+        l2 = new JLabel("Password");
+        l2.setBounds(40, 70, 100, 30);
+        add(l2);
+        
+        t1 = new JTextField();
+        t1.setBounds(150, 20, 150, 30);
+        add(t1);
+        
+        setLayout(null);
         setBounds(500, 300, 600, 400);
         setVisible(true);
     }
